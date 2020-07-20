4fb0f08a64d456d5079b666f34065713196627de
MrG-bit <ggggg243.channel@gmail.com>
2020-07-20 18:55:01 +1000

    Added ModGlobalItem just in case I want to use it in the future.

---
+++ Items/ModGlobalItem.cs
@@ -0,0 +1,18 @@
+﻿///<summary>
+/// File: ModGlobalItem.cs
+/// Last Updated: 2020-07-20
+/// Author: MRG-bit
+/// Description: Change things for every Item in the game
+///</summary>
+
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CrowdControlMod.Items
+{
+    public class ModGlobalItem : GlobalItem
+    {
+
+
+    }
+}
--- Projectiles/ModGlobalProjectile.cs
+++ Projectiles/ModGlobalProjectile.cs
@@ -7,8 +7,6 @@
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Runtime.InteropServices;
 using Terraria;
 using Terraria.ModLoader;
 
